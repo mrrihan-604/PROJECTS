40fbc70fe8f921f9063baf7cb62850e0f98a5ab1
mrrihan-604 <mrrihan604@gmail.com>
2026-07-07 15:59:06 +0530

    Tuple unpacking

---
+++ emp_record_unpack.py
@@ -0,0 +1,13 @@
+print("Employee Record Unpacking")
+emp_record=("Rk001","Rihan",25,"Software Engineer","100000")
+
+# Unpacking employee record
+emp_id,name,age,designation,salary=emp_record
+
+# printing employee records
+print("--Empolyee Detials--")
+print("Employee ID:",emp_id)
+print("Employee Name:",name)
+print("Employee Age:",age)
+print("Employee Designation:",designation)
+print("Employee Salary:",salary)
