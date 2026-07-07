f660eba9676e280e80086b4ec838b31cdfad18ef
mrrihan-604 <mrrihan604@gmail.com>
2026-07-07 16:46:40 +0530

    Added List Tuple Mutability program

---
+++ List_Tuple_Mutablity.py
@@ -0,0 +1,22 @@
+# The program helps to represent list is mutable and tuple is immutable
+# Initalixig tasks list
+task_queue = ["Review PR #102", "Fix login bug", "Update documentation"]
+
+# Modifying tasks list using append and pop
+task_queue.append("Prepare for sprint planning") 
+completed_task = task_queue.pop(0)                
+
+print(f"Remaining Task Queue: {task_queue}")
+
+office_branches_logs = [
+    (12.9716, 77.5946),  # Bengaluru Office 📍
+    (13.0827, 80.2707),  # Chennai Hub 📍
+]
+
+print(f"Office Locations Coordinates: {office_branches_logs}")
+
+# Tring to modify Tuple
+try:
+    office_branches_logs[0]=(14.8965, 82.7554)
+except TypeError as error:
+    print(error)
