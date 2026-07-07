60ab3dea207cc1a27f535b1c031129fc96e6a596
mrrihan-604 <mrrihan604@gmail.com>
2026-07-07 17:24:44 +0530

    Added Comments

--- List_Tuple_Mutablity.py
+++ List_Tuple_Mutablity.py
@@ -2,6 +2,7 @@
 # Initalixig tasks list
 task_queue = ["Review PR #102", "Fix login bug", "Update documentation"]
 
+# List is mutable so it can be modified
 # Modifying tasks list using append and pop
 task_queue.append("Prepare for sprint planning") 
 completed_task = task_queue.pop(0)                
@@ -15,7 +16,7 @@ office_branches_logs = [
 
 print(f"Office Locations Coordinates: {office_branches_logs}")
 
-# Tring to modify Tuple
+# Tring to modify Tuple but tuple is immutabe so it cannot be modified so it will throw exception
 try:
     office_branches_logs[0]=(14.8965, 82.7554)
 except TypeError as error:
